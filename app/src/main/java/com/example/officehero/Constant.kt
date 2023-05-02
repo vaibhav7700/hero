c3e32edba3eb1432b1ec5a380ea3398599557893
vaibhav7700 <vaibhavs0700@gmail.com>
2023-05-02 22:02:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/officehero/Constant.kt
@@ -0,0 +1,11 @@
+package com.example.officehero
+
+import com.intuit.sdp.BuildConfig
+
+interface Constant {
+
+    companion object {
+
+
+    }
+}
---
+++ app/src/main/java/com/example/officehero/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.officehero.login
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel (application: Application): ViewModel() {
+
+
+}
